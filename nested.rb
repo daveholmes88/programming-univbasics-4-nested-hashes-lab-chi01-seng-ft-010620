674b2f26e34040f0cd1222dd6bb0ce34849cbe7f
David Holmes <phoenix1988@gmail.com>
2019-12-24 15:26:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,6 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages] = ["Smalltalk", "LISP", "Assembly"]
-		 return programmer_hash 
+		 programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+		 return programmer_hash
 end
