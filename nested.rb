e7ed9da3831e9abaea589c2da8c562d6a71c32a8
David Holmes <phoenix1988@gmail.com>
2019-12-24 15:02:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,10 +64,10 @@ def adding_matz
 
 	programmer_hash =
  		{
-			#	:yukihoro_matsumoto => {
-				#	:known_for => "Ruby",
-				#	:languages => ["LISP" "C"]
-			#	}
+				:yukihoro_matsumoto => {
+					:known_for => "Ruby",
+					:languages => ["LISP" "C"]
+				}
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -81,8 +81,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+	#	 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+	#	 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
 
 
 end
